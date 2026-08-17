eb3ebe8f97ae9b82808357e9269b61a437aa81cc
naroah <givefacegod@gmail.com>
2026-08-17 12:38:19 +0800

    refactor: Integrate core game systems and expand flow controller logic

--- godot/core/run/run_context.gd
+++ godot/core/run/run_context.gd
@@ -9,6 +9,19 @@ var market_system: MarketSystem
 var policy_system: PolicySystem
 var inflation_system: InflationSystem
 var parliament_system: ParliamentSystem
+var race_system: RaceSystem
+var political_trust_system: PoliticalTrustSystem
+var draft_bill_system: DraftBillSystem
+var vote_system: VoteSystem
+var event_system: EventSystem
+var constitution_system: ConstitutionSystem
+var collapse_system: CollapseSystem
+var annual_settlement_system: AnnualSettlementSystem
+var race_definitions: Array[RaceDefinition] = []
+var interest_groups: Array[InterestGroupDefinition] = []
+var event_definitions: Array[EventDefinition] = []
+var constitution_articles: Array[ConstitutionArticleDefinition] = []
+var automatic_draw_count: int = 3
 
 
 func setup(
@@ -19,7 +32,15 @@ func setup(
 	run_market_system: MarketSystem,
 	run_policy_system: PolicySystem,
 	run_inflation_system: InflationSystem,
-	run_parliament_system: ParliamentSystem
+	run_parliament_system: ParliamentSystem,
+	run_race_system: RaceSystem,
+	run_political_trust_system: PoliticalTrustSystem,
+	run_draft_bill_system: DraftBillSystem,
+	run_vote_system: VoteSystem,
+	run_event_system: EventSystem,
+	run_constitution_system: ConstitutionSystem,
+	run_collapse_system: CollapseSystem,
+	run_annual_settlement_system: AnnualSettlementSystem
 ) -> void:
 	state = run_state
 	time_system = run_time_system
@@ -29,3 +50,11 @@ func setup(
 	policy_system = run_policy_system
 	inflation_system = run_inflation_system
 	parliament_system = run_parliament_system
+	race_system = run_race_system
+	political_trust_system = run_political_trust_system
+	draft_bill_system = run_draft_bill_system
+	vote_system = run_vote_system
+	event_system = run_event_system
+	constitution_system = run_constitution_system
+	collapse_system = run_collapse_system
+	annual_settlement_system = run_annual_settlement_system
--- godot/core/run/run_session.gd
+++ godot/core/run/run_session.gd
@@ -10,7 +10,32 @@ var market_system: MarketSystem
 var policy_system: PolicySystem
 var inflation_system: InflationSystem
 var parliament_system: ParliamentSystem
+var race_system: RaceSystem
+var political_trust_system: PoliticalTrustSystem
+var draft_bill_system: DraftBillSystem
+var vote_system: VoteSystem
+var event_system: EventSystem
+var constitution_system: ConstitutionSystem
+var collapse_system: CollapseSystem
+var annual_settlement_system: AnnualSettlementSystem
 var flow_controller: FlowController
+var race_definitions: Array[RaceDefinition] = []
+var interest_groups: Array[InterestGroupDefinition] = []
+var event_definitions: Array[EventDefinition] = []
+var constitution_articles: Array[ConstitutionArticleDefinition] = []
+var automatic_draw_count: int = 3
+
+
+func configure_content(
+	races: Array[RaceDefinition],
+	groups: Array[InterestGroupDefinition],
+	events: Array[EventDefinition] = [],
+	articles: Array[ConstitutionArticleDefinition] = []
+) -> void:
+	race_definitions = races
+	interest_groups = groups
+	event_definitions = events
+	constitution_articles = articles
 
 
 func start_new_run() -> void:
@@ -23,6 +48,14 @@ func start_new_run() -> void:
 	policy_system = PolicySystem.new()
 	inflation_system = InflationSystem.new()
 	parliament_system = ParliamentSystem.new()
+	race_system = RaceSystem.new()
+	political_trust_system = PoliticalTrustSystem.new()
+	draft_bill_system = DraftBillSystem.new()
+	vote_system = VoteSystem.new()
+	event_system = EventSystem.new()
+	constitution_system = ConstitutionSystem.new()
+	collapse_system = CollapseSystem.new()
+	annual_settlement_system = AnnualSettlementSystem.new()
 
 	context = RunContext.new()
 	context.setup(
@@ -33,8 +66,29 @@ func start_new_run() -> void:
 		market_system,
 		policy_system,
 		inflation_system,
-		parliament_system
+		parliament_system,
+		race_system,
+		political_trust_system,
+		draft_bill_system,
+		vote_system,
+		event_system,
+		constitution_system,
+		collapse_system,
+		annual_settlement_system
 	)
+	context.race_definitions = race_definitions
+	context.interest_groups = interest_groups
+	context.event_definitions = event_definitions
+	context.constitution_articles = constitution_articles
+	context.automatic_draw_count = automatic_draw_count
+	constitution_system.initialize(state, constitution_articles)
+	race_system.initialize_races(state, race_definitions)
+	race_system.recalculate_all_seat_counts(state)
+	constitution_system.apply_annual_seat_corrections(state)
+	var groups := constitution_system.get_effective_groups(state, interest_groups)
+	parliament_system.rebuild_all_rows(state, groups)
+	constitution_system.apply_special_fixed_relations(state)
+	constitution_system.update_petition_count(state)
 
 	flow_controller = FlowController.new()
 	flow_controller.setup(context)
@@ -54,3 +108,11 @@ func print_current_date() -> void:
 
 func enact_bill(draft: DraftBillState) -> void:
 	flow_controller.enact_bill(draft)
+
+
+func submit_draft() -> VoteResultState:
+	return flow_controller.submit_draft(state.draft_bill)
+
+
+func revise_constitution(article: ConstitutionArticleDefinition) -> bool:
+	return constitution_system.revise(context, article)
