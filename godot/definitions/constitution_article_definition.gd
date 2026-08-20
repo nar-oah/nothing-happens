7e4a00cb82917c3ce63e6adb09e9a35299d42f27
naroah <givefacegod@gmail.com>
2026-08-20 13:24:25 +0800

    refactor: Refactor constitution article and seat definitions

--- godot/definitions/constitution_article_definition.gd
+++ godot/definitions/constitution_article_definition.gd
@@ -1,26 +1,52 @@
 extends Resource
 class_name ConstitutionArticleDefinition
 
-enum ThresholdKind {
-	NONE,
-	RACE_SEAT_RATE,
-	GROUP_INFLUENCE_RATE,
-}
-
-@export var id: StringName
 @export var display_name: String
-@export var axis_id: StringName
-@export var direction: int = 0
-@export_range(0, 3, 1) var level: int = 0
+@export var race: RaceDefinition
 @export var is_initial: bool = false
-@export var is_terminal: bool = false
-@export var is_regulation: bool = false
-@export var prerequisite_id: StringName
-@export var threshold_kind: ThresholdKind = ThresholdKind.NONE
-@export var threshold_target_id: StringName
-@export_range(0.0, 1.0, 0.01) var threshold_rate: float = 0.0
-@export var flags: Array[StringName] = []
-@export var support_race_id: StringName
-@export var base_support_modifier: float = 0.0
+@export var prerequisite: ConstitutionArticleDefinition
+@export var seat_condition: ConstitutionSeatCondition
+
+@export_group("席位约束")
+@export_range(0.0, 1.0, 0.01) var race_min_seat_rate: float = 0.0
+@export_range(0.0, 1.0, 0.01) var race_max_seat_rate: float = 1.0
+
+@export_group("种族运行参数")
+@export_range(0.0, 1.0, 0.01) var expectation_growth_rate: float = 0.0
+@export_range(0.0, 1.0, 0.01) var visit_probability: float = 0.0
+
+@export_group("内容")
+@export var policies: Array[PolicyDefinition] = []
 @export var influence_rules: Array[ConstitutionInfluenceRule] = []
-@export var unlocked_policies: Array[PolicyDefinition] = []
+
+
+func can_activate(context) -> bool:
+	if prerequisite != null and not context.state.constitution.was_clicked(prerequisite):
+		return false
+	return seat_condition == null or seat_condition.is_met(context)
+
+
+func apply_runtime(context) -> void:
+	if race == null:
+		return
+	var race_state = context.state.get_race(race)
+	if race_state == null:
+		return
+	race_state.expectation_growth_rate = expectation_growth_rate
+	race_state.visit_probability = visit_probability
+
+
+func on_activate(_context) -> void:
+	pass
+
+
+func on_month_start(_context) -> void:
+	pass
+
+
+func on_year_settlement(_context) -> void:
+	pass
+
+
+func modify_vote(_vote_context) -> void:
+	pass
--- godot/definitions/constitution_influence_rule.gd
+++ godot/definitions/constitution_influence_rule.gd
@@ -1,28 +1,13 @@
 extends Resource
 class_name ConstitutionInfluenceRule
 
-enum Priority {
-	TERMINAL,
-	MERGER,
-	FIXED,
-	LIMIT,
+enum Mode {
+	MINIMUM,
+	MAXIMUM,
 	TARGET,
 }
 
-enum Action {
-	REMOVE_GROUP,
-	MERGE_GROUP,
-	LOCALIZE,
-	FIX_RACE_TO_GROUP,
-	GROUP_MINIMUM,
-	GROUP_MAXIMUM,
-	GROUP_TARGET,
-}
-
-@export var priority: Priority = Priority.FIXED
-@export var action: Action = Action.FIX_RACE_TO_GROUP
-@export var race_id: StringName
-@export var source_group_id: StringName
-@export var target_group_id: StringName
+@export var race: RaceDefinition
+@export var interest_group: InterestGroupDefinition
+@export var mode: Mode = Mode.TARGET
 @export_range(0.0, 1.0, 0.01) var rate: float = 0.0
-@export var local_group_prefix: StringName = &"local"
