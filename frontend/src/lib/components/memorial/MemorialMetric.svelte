960fac6a1ab63994f19f2ae215a6ceb73253e5be
naroah <givefacegod@gmail.com>
2026-08-20 23:31:10 +0800

    chore: Refactor MemorialMetric component and update UnoCSS configuration

--- frontend/src/lib/components/memorial/MemorialMetric.svelte
+++ frontend/src/lib/components/memorial/MemorialMetric.svelte
@@ -1,46 +1,91 @@
 <script lang="ts">
 	import type { MemorialMetricData } from './memorial';
-	type Props = { metric: MemorialMetricData };
-	let { metric }: Props = $props();
+	type Props = {
+		metric: MemorialMetricData;
+		isBottom?: boolean;
+		isColumn?: boolean;
+	};
+	let { metric, isBottom = false, isColumn = false }: Props = $props();
 	let { isReverse, symbol, text, value }: MemorialMetricData = $derived(metric);
 </script>
 
-<div class="flex items-center">
-	{#if isReverse}
+{#if isColumn && isBottom}
+	<div class="flex h-58 w-116 items-center">
 		<div
-			class="typo-data-metric-label-vertical mr-[-17px] flex w-[45px] shrink-0 flex-col justify-center [word-break:break-word] text-accent-amber-deep"
+			class:text-accent-amber-deep={isReverse}
+			class:text-ink-secondary={!isReverse}
+			class="typo-data-metric-label-large -mr-25 flex w-58 shrink-0 flex-col justify-center text-center [word-break:break-word]"
 		>
 			{text}
 		</div>
-		<div class="flex shrink-0 flex-col items-start justify-center bg-accent-amber-deep">
-			<div class="flex h-[15px] w-[17px] items-center justify-center">
-				<div class="typo-data-metric-sign rotate-90 whitespace-nowrap text-ink-secondary">
+		<div class="flex h-24 w-25 shrink-0 items-center justify-center">
+			<div class="rotate-90">
+				<div
+					class:bg-ink-secondary={isReverse}
+					class:bg-accent-amber-deep={!isReverse}
+					class="flex items-center justify-center"
+				>
+					<div
+						class:text-accent-amber-deep={isReverse}
+						class:text-ink-secondary={!isReverse}
+						class="typo-data-metric-value-tight whitespace-nowrap text-center [word-break:break-word]"
+					>
+						{value}
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+{:else if isBottom}
+	<div class:justify-end={isReverse} class="flex flex-col items-center">
+		<div
+			class:-mb-17={isReverse}
+			class:text-accent-amber-deep={isReverse}
+			class:text-ink-secondary={!isReverse}
+			class="typo-data-metric-label-vertical flex w-45 shrink-0 flex-col justify-center text-center [word-break:break-word]"
+		>
+			{text}
+		</div>
+		{#if isReverse}
+			<div class="flex shrink-0 items-center bg-accent-amber-deep">
+				<div class="typo-data-metric-value whitespace-nowrap text-ink-secondary">
+					{value}
+				</div>
+			</div>
+		{/if}
+	</div>
+{:else}
+	<div class="flex items-center">
+		<div
+			class:text-accent-amber-deep={isReverse}
+			class:text-ink-secondary={!isReverse}
+			class="typo-data-metric-label-vertical -mr-17 flex w-45 shrink-0 flex-col justify-center [word-break:break-word]"
+		>
+			{text}
+		</div>
+		<div
+			class:bg-accent-amber-deep={isReverse}
+			class:bg-ink-secondary={!isReverse}
+			class="flex shrink-0 flex-col items-start justify-center"
+		>
+			<div class="flex h-15 w-17 items-center justify-center">
+				<div
+					class:text-ink-secondary={isReverse}
+					class:text-accent-amber-deep={!isReverse}
+					class="typo-data-metric-sign rotate-90 whitespace-nowrap"
+				>
 					{symbol}
 				</div>
 			</div>
-			<div class="flex h-[16px] w-[17px] items-center justify-center">
-				<div class="typo-data-metric-value rotate-90 whitespace-nowrap text-ink-secondary">
+			<div class="flex h-16 w-17 items-center justify-center">
+				<div
+					class:text-ink-secondary={isReverse}
+					class:text-accent-amber-deep={!isReverse}
+					class="typo-data-metric-value rotate-90 whitespace-nowrap"
+				>
 					{value}
 				</div>
 			</div>
 		</div>
-	{:else}
-		<div
-			class="typo-data-metric-label-vertical mr-[-17px] flex w-[45px] shrink-0 flex-col justify-center [word-break:break-word] text-ink-secondary"
-		>
-			{text}
-		</div>
-		<div class="flex shrink-0 flex-col items-start justify-center">
-			<div class="flex h-[15px] w-[17px] items-center justify-center">
-				<div class="typo-data-metric-sign rotate-90 whitespace-nowrap text-accent-amber-deep">
-					{symbol}
-				</div>
-			</div>
-			<div class="flex h-[16px] w-[17px] items-center justify-center">
-				<div class="typo-data-metric-value rotate-90 whitespace-nowrap text-accent-amber-deep">
-					{value}
-				</div>
-			</div>
-		</div>
-	{/if}
-</div>
+	</div>
+{/if}
