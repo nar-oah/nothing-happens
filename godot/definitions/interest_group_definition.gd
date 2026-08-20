568128c9d8e8d0899f60ed72a6e88cbb90bdb0d4
naroah <givefacegod@gmail.com>
2026-08-20 13:40:08 +0800

    refactor: Increase default base_column_weight to 3

--- godot/definitions/interest_group_definition.gd
+++ godot/definitions/interest_group_definition.gd
@@ -2,7 +2,7 @@ extends Resource
 class_name InterestGroupDefinition
 
 @export var display_name: String
-@export_range(1, 999, 1) var base_column_weight: int = 1
+@export_range(1, 999, 1) var base_column_weight: int = 3
 
 @export_group("希望降低")
 @export var decrease_tax: bool = false
