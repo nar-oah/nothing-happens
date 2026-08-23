854084c37491605b1e17df1ff1146b1c04364e81
naroah <givefacegod@gmail.com>
2026-08-24 00:58:58 +0800

    refactor: Define Window and IPC types for CEF integration

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,6 +1,16 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
+	type CefIpcListener = (message: string) => void;
+
+	interface Window {
+		sendIpcMessage?: (message: string) => void;
+		ipcMessage?: {
+			addListener(listener: CefIpcListener): void;
+			removeListener?(listener: CefIpcListener): void;
+		};
+	}
+
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
