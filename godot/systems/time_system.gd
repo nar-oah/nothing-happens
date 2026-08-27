c5210ce785319932ff92af3bef7037146dd88eb8
naroah <32189487+nar-oah@users.noreply.github.com>
2026-08-27 11:09:01 +0800

    feat: add constitution month zero transition

--- godot/systems/time_system.gd
+++ godot/systems/time_system.gd
@@ -3,8 +3,8 @@ class_name TimeSystem
 
 
 func advance_month(state: RunState) -> void:
-	state.month += 1
-
-	if state.month > 12:
-		state.month = 1
+	if state.month == 12:
 		state.year += 1
+		state.month = 0
+		return
+	state.month += 1
