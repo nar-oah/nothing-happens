9899e6d519264fe66d66ff748574ef1885167b96
naroah <32189487+nar-oah@users.noreply.github.com>
2026-08-28 18:18:17 +0800

    refactor: make proposal digestion progress random

--- godot/states/active_proposal_state.gd
+++ godot/states/active_proposal_state.gd
@@ -3,21 +3,27 @@ class_name ActiveProposalState
 
 var proposal: ProposalInstance
 var digested_months: int = 0
+var digestion_progress: float = 0.0
 
 
 func _init(source_proposal: ProposalInstance) -> void:
 	proposal = source_proposal
 
 
-func advance_month() -> void:
-	if not is_fully_digested():
-		digested_months += 1
+func advance_month(random_system: RandomSystem, variance: float) -> void:
+	if is_fully_digested() or proposal == null:
+		return
+	digested_months += 1
+	var base_step := 1.0 / float(maxi(proposal.lag_months, 1))
+	var clamped_variance := clampf(variance, 0.0, 1.0)
+	var multiplier := random_system.random_float(
+		1.0 - clamped_variance, 1.0 + clamped_variance
+	)
+	digestion_progress = minf(1.0, digestion_progress + base_step * multiplier)
 
 
 func get_digestion_progress() -> float:
-	if proposal == null:
-		return 0.0
-	return minf(1.0, float(digested_months) / float(maxi(proposal.lag_months, 1)))
+	return clampf(digestion_progress, 0.0, 1.0)
 
 
 func is_fully_digested() -> bool:
