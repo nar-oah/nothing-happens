ffaca94e1ebcab2af2555e2eb3321dca4dd3671d
naroah <32189487+nar-oah@users.noreply.github.com>
2026-08-28 18:00:30 +0800

    refactor: add constitution row definition

---
+++ godot/definitions/constitution_row_definition.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name ConstitutionRowDefinition
+
+@export var id: StringName
+@export var display_name: String
+@export var race: RaceDefinition
+@export_range(0, 999, 1) var display_order: int = 0
+@export var ignores_column_unlocks: bool = false
+@export var free_navigation: bool = false
