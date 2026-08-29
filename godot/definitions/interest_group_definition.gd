059cf7d74d856d9ab4ea1d7e8d783ff95b871b59
naroah <32189487+nar-oah@users.noreply.github.com>
2026-08-29 13:44:12 +0800

    refactor: rename interest group metric fields

--- godot/definitions/interest_group_definition.gd
+++ godot/definitions/interest_group_definition.gd
@@ -7,10 +7,10 @@ class_name InterestGroupDefinition
 
 @export_group("希望降低")
 @export var decrease_tax: bool = false
-@export var decrease_price: bool = false
-@export var decrease_wage: bool = false
+@export var decrease_consumption: bool = false
+@export var decrease_production: bool = false
 @export var decrease_employment: bool = false
-@export var decrease_trade: bool = false
+@export var decrease_investment: bool = false
 
 
 func get_stance(metric: Metric.Id) -> Metric.Direction:
@@ -29,12 +29,26 @@ func _decreases_metric(metric: Metric.Id) -> bool:
 	match metric:
 		Metric.Id.TAX:
 			return decrease_tax
-		Metric.Id.PRICE:
-			return decrease_price
-		Metric.Id.WAGE:
-			return decrease_wage
+		Metric.Id.CONSUMPTION:
+			return decrease_consumption
+		Metric.Id.PRODUCTION:
+			return decrease_production
 		Metric.Id.EMPLOYMENT:
 			return decrease_employment
-		Metric.Id.TRADE:
-			return decrease_trade
+		Metric.Id.INVESTMENT:
+			return decrease_investment
+	return false
+
+
+func _set(property: StringName, value: Variant) -> bool:
+	match property:
+		&"decrease_price":
+			decrease_consumption = bool(value)
+			return true
+		&"decrease_wage":
+			decrease_production = bool(value)
+			return true
+		&"decrease_trade":
+			decrease_investment = bool(value)
+			return true
 	return false
