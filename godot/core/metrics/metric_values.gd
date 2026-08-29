d4bca79419779447fe7aa5f1642c0e429d5a82e4
naroah <32189487+nar-oah@users.noreply.github.com>
2026-08-29 13:42:55 +0800

    refactor: rename metric value fields

--- godot/core/metrics/metric_values.gd
+++ godot/core/metrics/metric_values.gd
@@ -2,24 +2,24 @@ extends RefCounted
 class_name MetricValues
 
 var tax: int = 0
-var price: int = 0
-var wage: int = 0
+var consumption: int = 0
+var production: int = 0
 var employment: int = 0
-var trade: int = 0
+var investment: int = 0
 
 
 func get_value(metric: Metric.Id) -> int:
 	match metric:
 		Metric.Id.TAX:
 			return tax
-		Metric.Id.PRICE:
-			return price
-		Metric.Id.WAGE:
-			return wage
+		Metric.Id.CONSUMPTION:
+			return consumption
+		Metric.Id.PRODUCTION:
+			return production
 		Metric.Id.EMPLOYMENT:
 			return employment
-		Metric.Id.TRADE:
-			return trade
+		Metric.Id.INVESTMENT:
+			return investment
 		_:
 			push_error("Unknown metric: %s" % metric)
 			return 0
@@ -29,33 +29,31 @@ func set_value(metric: Metric.Id, value: int) -> void:
 	match metric:
 		Metric.Id.TAX:
 			tax = value
-		Metric.Id.PRICE:
-			price = value
-		Metric.Id.WAGE:
-			wage = value
+		Metric.Id.CONSUMPTION:
+			consumption = value
+		Metric.Id.PRODUCTION:
+			production = value
 		Metric.Id.EMPLOYMENT:
 			employment = value
-		Metric.Id.TRADE:
-			trade = value
+		Metric.Id.INVESTMENT:
+			investment = value
 		_:
 			push_error("Unknown metric: %s" % metric)
 
 
 func apply_delta(delta: MetricVector) -> void:
 	tax += delta.tax
-	price += delta.price
-	wage += delta.wage
+	consumption += delta.consumption
+	production += delta.production
 	employment += delta.employment
-	trade += delta.trade
+	investment += delta.investment
 
 
 func copy() -> MetricValues:
 	var result := MetricValues.new()
-
 	result.tax = tax
-	result.price = price
-	result.wage = wage
+	result.consumption = consumption
+	result.production = production
 	result.employment = employment
-	result.trade = trade
-
+	result.investment = investment
 	return result
