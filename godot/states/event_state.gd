c729df2d8b53293ec973597393cbab9229df68f3
naroah <32189487+nar-oah@users.noreply.github.com>
2026-09-05 13:30:50 +0800

    feat: generalize event requirement source

--- godot/states/event_state.gd
+++ godot/states/event_state.gd
@@ -1,6 +1,11 @@
 extends RefCounted
 class_name EventState
 
+enum RequirementKind {
+	METRIC,
+	INTEREST_GROUP_PROPOSALS,
+}
+
 enum Phase {
 	WORSENING,
 	PAUSED,
@@ -10,7 +15,9 @@ enum Phase {
 }
 
 var race: RaceDefinition
+var requirement_kind: int = RequirementKind.METRIC
 var metric: Metric.Id = Metric.Id.TAX
+var interest_group: InterestGroupDefinition
 var baseline_value: int = 0
 var full_target: int = 0
 var growth_progress: float = 0.0
@@ -25,12 +32,16 @@ func _init(
 	source_race: RaceDefinition = null,
 	source_metric: Metric.Id = Metric.Id.TAX,
 	source_baseline: int = 0,
-	source_target: int = 0
+	source_target: int = 0,
+	source_requirement_kind: int = RequirementKind.METRIC,
+	source_interest_group: InterestGroupDefinition = null
 ) -> void:
 	race = source_race
 	metric = source_metric
 	baseline_value = source_baseline
 	full_target = source_target
+	requirement_kind = source_requirement_kind
+	interest_group = source_interest_group
 
 
 func is_active() -> bool:
