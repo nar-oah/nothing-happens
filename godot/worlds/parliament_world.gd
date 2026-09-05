ab3927b81f48cc6cea1cedea58d2afffbf840805
naroah <givefacegod@gmail.com>
2026-09-05 10:57:43 +0800

    feat: Add camera movement and layout change signals to parliament world

--- godot/worlds/parliament_world.gd
+++ godot/worlds/parliament_world.gd
@@ -1,7 +1,13 @@
 extends Node2D
 class_name ParliamentWorld
 
+signal layout_changed(parliament_seat_anchors: Array[Dictionary])
+
 @export var seat_scene: PackedScene
+@export var camera_move_speed: float = 1600.0
+@export var camera_left_boundary: float = 2400.0
+@export var camera_right_boundary: float = 7600.0
+@onready var camera: Camera2D = $Camera2D
 @onready var seats_root: Node2D = $Seats
 
 var seat_races: Array[RaceDefinition] = []
@@ -9,15 +15,29 @@ var seats: Array[ParliamentSeat] = []
 
 
 func _ready() -> void:
-	for child in seats_root.get_children():
-		if child is ParliamentSeat:
-			seats.append(child)
+	_collect_seats(seats_root)
 	seats.sort_custom(
 		func(first: ParliamentSeat, second: ParliamentSeat) -> bool:
 			return first.seat_index < second.seat_index
 	)
+	get_viewport().size_changed.connect(_on_viewport_size_changed)
 	if not seat_races.is_empty():
 		_refresh_seats()
+
+
+func _process(delta: float) -> void:
+	var direction := Input.get_axis("parliament_left", "parliament_right")
+	if is_zero_approx(direction):
+		return
+	var next_x := clampf(
+		camera.position.x + direction * camera_move_speed * delta,
+		camera_left_boundary,
+		camera_right_boundary
+	)
+	if is_equal_approx(next_x, camera.position.x):
+		return
+	camera.position.x = next_x
+	_emit_layout_changed()
 
 
 func set_seat_races(value: Array[RaceDefinition]) -> void:
@@ -37,7 +57,7 @@ func get_seat_anchors() -> Array[Dictionary]:
 func _refresh_seats() -> void:
 	while seats.size() > seat_races.size():
 		var removed: ParliamentSeat = seats.pop_back()
-		seats_root.remove_child(removed)
+		removed.get_parent().remove_child(removed)
 		removed.queue_free()
 	while seats.size() < seat_races.size():
 		var seat := seat_scene.instantiate() as ParliamentSeat
@@ -46,3 +66,20 @@ func _refresh_seats() -> void:
 	for index in range(seat_races.size()):
 		seats[index].seat_index = index
 		seats[index].set_race(seat_races[index])
+	_emit_layout_changed()
+
+
+func _collect_seats(parent: Node) -> void:
+	for child in parent.get_children():
+		if child is ParliamentSeat:
+			seats.append(child)
+		_collect_seats(child)
+
+
+func _on_viewport_size_changed() -> void:
+	_emit_layout_changed()
+
+
+func _emit_layout_changed() -> void:
+	camera.force_update_scroll()
+	layout_changed.emit(get_seat_anchors())
