a9cc595ff794fa2fe29483acd19784fe5519564d
naroah <givefacegod@gmail.com>
2026-09-06 10:56:45 +0800

    feat: Add hover portrait support to race definitions

--- godot/definitions/biyi_race_definition.gd
+++ godot/definitions/biyi_race_definition.gd
@@ -3,9 +3,16 @@ class_name BiyiRaceDefinition
 
 @export_group("阴阳半身")
 @export var yang_portrait: Texture2D
+@export var yang_hover_portrait: Texture2D
 
 
 func get_portrait(month: int) -> Texture2D:
 	if month % 2 == 0 and yang_portrait != null:
 		return yang_portrait
 	return portrait
+
+
+func get_hover_portrait(month: int) -> Texture2D:
+	if month % 2 == 0 and yang_portrait != null:
+		return yang_hover_portrait
+	return hover_portrait
