3e9f1b9c0f810922a5339f9eb560ff0483c45c84
naroah <givefacegod@gmail.com>
2026-09-06 11:13:13 +0800

    fix: Refactor mouse event handlers to use stored textures

--- godot/worlds/interactables/office_visitor.gd
+++ godot/worlds/interactables/office_visitor.gd
@@ -4,15 +4,19 @@ class_name OfficeVisitor
 signal clicked
 
 @export_range(0.1, 1.0, 0.05) var hitbox_ratio: float = 0.8
+var normal_texture: Texture2D
+var hover_texture: Texture2D
 @onready var visual: Sprite2D = $Visual
 @onready var hitbox: CollisionShape2D = $Hitbox
 
 
 func set_portrait(portrait: Texture2D, hover: Texture2D) -> void:
 	visual.texture = portrait
+	normal_texture = portrait
+	hover_texture = hover
 	_update_hitbox()
-	mouse_entered.connect(_on_mouse_entered.call(portrait, hover))
-	mouse_exited.connect(_on_mouse_exited.call(portrait))
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
 
 
 func _update_hitbox() -> void:
@@ -20,12 +24,12 @@ func _update_hitbox() -> void:
 	shape.size = visual.texture.get_size() * hitbox_ratio
 
 
-func _on_mouse_entered(portrait: Texture2D, hover: Texture2D) -> void:
-	visual.texture = hover if is_instance_of(hover, Texture2D) else portrait
+func _on_mouse_entered() -> void:
+	visual.texture = hover_texture if is_instance_of(hover_texture, Texture2D) else normal_texture
 
 
-func _on_mouse_exited(portrait: Texture2D) -> void:
-	visual.texture = portrait
+func _on_mouse_exited() -> void:
+	visual.texture = normal_texture
 
 
 func _input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
